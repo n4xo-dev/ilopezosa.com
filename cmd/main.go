493c5054efa6a85d92a4c23300a8140052782f81
Ignacio Lopezosa Serrano <ilopezosaserrano@gmail.com>
2023-11-24 23:38:40 +0100

    Styles in file, processing contact form, & contact-response cards

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -17,6 +20,20 @@ func main() {
 	})
 	app.Get("/contact", func(c *fiber.Ctx) error {
 		return c.SendFile("./web/contact.html")
+	})
+
+	app.Post("/contact", func(c *fiber.Ctx) error {
+		name := c.FormValue("name")
+		email := c.FormValue("email")
+		message := c.FormValue("message")
+
+		// Send email
+		// ...
+		fmt.Println(name, email, message)
+		if strings.ToLower(name) == "error" {
+			return c.SendFile("./web/cards/error.html")
+		}
+		return c.SendFile("./web/cards/success.html")
 	})
 
 	app.Listen(":3000")
